1b530673f242e801fa80db0294b355fa38b95c92
song-eunhye <dmsgp8292@gmail.com>
2024-04-09 16:25:33 +0900

    92강 미니 실습

--- App_89강.js
+++ App_89강.js
@@ -16,6 +16,11 @@ const faqs = [
   },
 ];
 
+/*
+    89강 미니 실습 과제
+    92강 미니 실습 과제 - 하나의 카드만 열리도록 변경 & children prop 사용
+*/
+
 export default function App() {
   return (
     <div>
@@ -25,32 +30,45 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [isOpen, setIsOpen] = useState(null);
+  // 내가 혼자 했던 방식. 강의에선 AccordionItem 안에 함수를 만듦.
+  // function handleOpen(title) {
+  //   if (isOpen === title) {
+  //     setIsOpen(null);
+  //     return;
+  //   }
+  //   setIsOpen(title);
+  // }
   return (
     <div className="accordion">
       {data.map((item, i) => (
         <AccordionItem
           title={item.title}
-          text={item.text}
           index={i}
           key={item.title}
-        />
+          curOpen={isOpen}
+          onOpen={setIsOpen}
+        >
+          {item.text}
+        </AccordionItem>
       ))}
     </div>
   );
 }
 
-function AccordionItem({ title, text, index }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ title, index, curOpen, onOpen, children }) {
+  const isOpen = curOpen === index;
+
+  function handleOpen() {
+    onOpen(isOpen ? null : index);
+  }
 
   return (
-    <div
-      className={`item ${isOpen ? "open" : ""}`}
-      onClick={() => setIsOpen(!isOpen)}
-    >
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleOpen}>
       <span className="number">{index < 9 ? `0${index + 1}` : index + 1}</span>
       <p className="title">{title}</p>
       <p className="icon">+</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
