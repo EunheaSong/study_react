e466339cc232c234bca22b97019114f26da73e73
eunhyes <dmsgp8292@gmail.com>
2024-04-17 15:24:27 +0900

    121강 실습과제 강의 코드 추가

--- practice-task/src/App_121강.js
+++ practice-task/src/App_121강.js
@@ -34,28 +34,63 @@ export default function App() {
     </div>
   );
 }
+//self version
+// function TextExpander({
+//   children,
+//   className = "",
+//   buttonColor = "blue",
+//   collapsedNumWords = 10,
+//   expandButtonText = "Show more",
+//   collapseButtonText = "Show less",
+//   expanded = false,
+// }) {
+//   const [isOpen, setIsOpen] = useState(expanded);
+//   const viewString = children.split(" ").slice(0, collapsedNumWords).join(" ");
+//   function onOpen() {
+//     setIsOpen(!isOpen);
+//   }
+//   return (
+//     <div className={className}>
+//       {isOpen ? children : viewString + "..."}
+//       <span role="button" onClick={onOpen} style={{ color: buttonColor }}>
+//         {" "}
+//         {isOpen ? collapseButtonText : expandButtonText}
+//       </span>
+//     </div>
+//   );
+// }
 
+//강의 버전
 function TextExpander({
-  children,
-  className = "",
-  buttonColor = "blue",
   collapsedNumWords = 10,
   expandButtonText = "Show more",
   collapseButtonText = "Show less",
+  buttonColor = "#1f09cd",
   expanded = false,
+  className,
+  children,
 }) {
-  const [isOpen, setIsOpen] = useState(expanded);
-  const viewString = children.split(" ").slice(0, collapsedNumWords).join(" ");
-  function onOpen() {
-    setIsOpen(!isOpen);
-  }
+  const [isExpanded, setIsExpanded] = useState(expanded);
+
+  const displayText = isExpanded
+    ? children
+    : children.split(" ").slice(0, collapsedNumWords).join(" ") + "...";
+
+  const buttonStyle = {
+    background: "none",
+    border: "none",
+    font: "inherit",
+    cursor: "pointer",
+    marginLeft: "6px",
+    color: buttonColor,
+  };
+
   return (
     <div className={className}>
-      {isOpen ? children : viewString + "..."}
-      <span role="button" onClick={onOpen} style={{ color: buttonColor }}>
-        {" "}
-        {isOpen ? collapseButtonText : expandButtonText}
-      </span>
+      <span>{displayText}</span>
+      <button onClick={() => setIsExpanded((exp) => !exp)} style={buttonStyle}>
+        {isExpanded ? collapseButtonText : expandButtonText}
+      </button>
     </div>
   );
 }
